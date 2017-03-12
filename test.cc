908253fefe12681914be78306e2616ee745a274b
robbenychen <robbenychen@gmail.com>
2017-03-12 13:18:25 +0800

    test2

--- test.cc
+++ test.cc
@@ -1,11 +1,23 @@
 #include<iostream>
-
+#include<memory>
 
 using namespace std;
+struct A
+{
+
+    int a;
+    int b;
+};
 
 
 
 int main()
 {
+    auto a = make_shared<A>();
+
+    a->a = 1;
+
+
     cout<<"hello world" <<endl;
+    cout<< a->a  <<endl;
 }
